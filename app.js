89b82a0831323979ae2718b325e7f074986fc107
Alcidescu <alcidescallasi@gmail.com>
2025-04-25 12:35:43 -0500

    cambio1

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/api/configuracions', configuracionRouter);
 
 app.use('/api/auth', authRouter);
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
   console.log(`🚀 Servidor backend corriendo en puerto ${PORT}`);
   await connectDB();
